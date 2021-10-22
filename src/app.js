084b6b358ad97645553a3d640572ccd89f2de7dc
AlfredoMarcelo <alfredo.supanta.26@gmail.com>
2021-10-22 18:01:50 +0000

    Ejercicio sin modificación

--- src/app.js
+++ src/app.js
@@ -7,5 +7,8 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombres = ["El", "Nuestro"];
+  let adjetivos = ["mejor", "gran", ""];
+  let = ["libro", "racoon"];
+  console.log("Hola alfredo!");
 };
