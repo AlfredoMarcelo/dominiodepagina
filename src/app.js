222e5866dc4eabeccd3c182f1bdb2cc057b6f2cb
AlfredoMarcelo <alfredo.supanta.26@gmail.com>
2021-10-28 21:11:59 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -7,8 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let pronombres = ["El", "Nuestro"];
-  let adjetivos = ["mejor", "gran", ""];
-  let = ["libro", "racoon"];
-  console.log("Hola alfredo!");
+  let pronombres = ["El", "Nuestro", "Tú"];
+  let adjetivos = ["mejor", "gran", "excelente", "unico"];
+  let sustantivos = ["libro", "juego", "partido", "codigo", "marcador"];
+  let dominios = ["com", "cl", "es", "col", "pe"];
+
+  for (let a of pronombres) {
+    for (let b of adjetivos) {
+      for (let c of sustantivos) {
+        for (let d of dominios) {
+          console.log(`${a}${b}${c}.${d}`);
+        }
+      }
+    }
+  }
 };
